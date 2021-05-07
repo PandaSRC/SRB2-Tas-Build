6d80890c6fa4cfab0c7bd4037ea1bd66d3243df1
Zenya <43818834+Zeritar@users.noreply.github.com>
2021-05-07 16:34:41 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-killall -9 srb29tas
-export LIBGME_CFLAGS=
-export LIBGME_LDFLAGS=-lgme
-make -C src/ LINUX64=1
-sudo cp /home/zenya/srb29tas/SRB2/bin/Linux64/Release/lsdl2srb2 /usr/games/SRB2/srb29tas
